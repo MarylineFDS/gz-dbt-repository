60dff9d15f7e27384a00b2271ef82e76d2a2cb88
Maryline Ferreira <maryline.ferreirads@gmail.com>
2026-02-05 11:26:56 +0000

    fin challenge 5

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,6 +3,9 @@ SELECT
   ,MAX(o.date_date) AS date_date
   ,SUM(o.revenue) AS revenue
   ,SUM(o.quantity) AS quantity
+  ,SUM(s.logcost) AS log_cost
+  ,SUM(s.ship_cost) AS ship_cost
+  ,SUM(s.shipping_fee) AS shipping_fee
   ,SUM(o.purchase_cost) AS purchase_cost
   ,SUM(o.margin) AS margin
   ,ROUND(SUM(o.margin+s.shipping_fee-s.logcost-s.ship_cost),2) AS operational_margin
