01fa44060641120c726f49908ad3d919b2f49ba9
Maryline Ferreira <maryline.ferreirads@gmail.com>
2026-02-05 09:47:37 +0000

    changes on tab

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,13 +1,12 @@
 SELECT
   s.orders_id
-  ,MAX(s.date_date) AS date_date
-  ,COUNT(s.products_id) AS nb_products
-  ,ROUND(SUM(s.revenue),2) AS revenue
-  ,ROUND(SUM(s.quantity),2) AS quantity
-  ,ROUND(SUM(p.purchase_price),2) AS purchase_price
-  ,ROUND(SUM(s.quantity*p.purchase_price),2) AS purchase_cost
-  ,ROUND(SUM(s.revenue-(s.quantity*p.purchase_price)),2) AS margin
+  ,s.date_date AS date_date
+  ,s.products_id AS nb_products
+  ,s.revenue AS revenue
+  ,s.quantity AS quantity
+  ,p.purchase_price AS purchase_price
+  ,ROUND(s.quantity*p.purchase_price,2) AS purchase_cost
+  ,ROUND(s.revenue-(s.quantity*p.purchase_price),2) AS margin
 FROM {{ref('stg_raw__sales')}} AS s
 LEFT JOIN{{ref('stg_raw__product')}} AS p
 ON s.products_id=p.products_id
-GROUP BY s.orders_id
