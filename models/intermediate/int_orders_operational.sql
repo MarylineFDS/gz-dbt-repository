ecd4f2d0b8d866bd4d88e269a1767c94c46bbd18
Maryline Ferreira <maryline.ferreirads@gmail.com>
2026-02-05 10:14:39 +0000

    operationnal

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+SELECT
+  o.orders_id AS orders_id
+  ,MAX(o.date_date) AS date_date
+  ,SUM(o.revenue) AS revenue
+  ,SUM(o.quantity) AS quantity
+  ,SUM(o.purchase_cost) AS purchase_cost
+  ,SUM(o.margin) AS margin
+  ,ROUND(SUM(o.margin+s.shipping_fee-s.logcost-s.ship_cost),2) AS operational_margin
+FROM {{ ref('int_orders_margin') }} AS o
+LEFT JOIN {{ ref('stg_raw__ship') }} AS s
+ON o.orders_id=s.orders_id
+GROUP BY o.orders_id
+ORDER BY o.orders_id DESC
