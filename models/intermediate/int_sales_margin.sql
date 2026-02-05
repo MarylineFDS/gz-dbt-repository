c3197cf412602e0b09567a6068df957d7ac921f7
Maryline Ferreira <maryline.ferreirads@gmail.com>
2026-02-05 09:43:08 +0000

    Calcul margin et purchase cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,13 @@
+SELECT
+  s.orders_id
+  ,MAX(s.date_date) AS date_date
+  ,COUNT(s.products_id) AS nb_products
+  ,ROUND(SUM(s.revenue),2) AS revenue
+  ,ROUND(SUM(s.quantity),2) AS quantity
+  ,ROUND(SUM(p.purchase_price),2) AS purchase_price
+  ,ROUND(SUM(s.quantity*p.purchase_price),2) AS purchase_cost
+  ,ROUND(SUM(s.revenue-(s.quantity*p.purchase_price)),2) AS margin
+FROM {{ref('stg_raw__sales')}} AS s
+LEFT JOIN{{ref('stg_raw__product')}} AS p
+ON s.products_id=p.products_id
+GROUP BY s.orders_id
